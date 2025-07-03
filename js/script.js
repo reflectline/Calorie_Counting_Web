d7be913abe668aa1287bc31e285081c3bc734265
Kiril <kirimmnaz@mail.ru>
2025-07-04 01:40:51 +0300

    test1

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 
 console.log('Hello World!');
 
+echo "# Calorie_Counting_Web" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/reflectline/Calorie_Counting_Web.git
+    git push -u origin main
+
+git remote add origin https://github.com/reflectline/Calorie_Counting_Web.git
+    git branch -M main
+git push -u origin main
